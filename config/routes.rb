96b957814f23b25cb2502cf27a675ec064eb8ec4
Jason Zerbe <jzerbe@vraidsys.com>
2015-08-01 16:07:37 +0000

    checkpoint after: rails generate controller Sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :notes
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
