e87250bb02f1f05bf3035f2a29dc17f6bad3d3e5
Jason Zerbe <jzerbe@vraidsys.com>
2015-08-02 02:44:17 +0000

    allow for logout from user page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
+    end
+
     def logged_in?
         current_user != nil
     end
