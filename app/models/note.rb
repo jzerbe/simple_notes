a1897f3c1538ceb1b21d16cccba543695a38f0ca
Jason Zerbe <jzerbe@vraidsys.com>
2015-08-02 02:30:31 +0000

    add activerecord hooks for future perm enforcement

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,17 @@
 class Note < ActiveRecord::Base
-  belongs_to :user
+    belongs_to :user
+
+    before_save :before_save
+    before_destroy :before_destroy
+    # http://api.rubyonrails.org/classes/ActiveRecord/Callbacks.html
+    # better to do this in the controller? hooks are here ...
+
+    private
+        def before_destroy
+            #TODO enforce ownership before delete
+        end
+
+        def before_save
+            #TODO enforce ownership before update
+        end
 end
